cb78dcb89a586fea7dd1fd625f540054da531932
zenglenn42 <gsnospampls@pobox.com>
2020-05-21 18:27:05 -0500

    add react demo logo splash div

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import { FaReact } from "react-icons/fa"
 
 class App extends Component {
-  // Initialize state
-  state = { passwords: [] }
-
-  // Fetch passwords after first mount
-  componentDidMount() {
-    this.getPasswords();
-  }
-
-  getPasswords = () => {
-    // Get the passwords and store them in state
-    fetch('/api/passwords')
-      .then(res => res.json())
-      .then(passwords => this.setState({ passwords }));
-  }
 
   render() {
-    const { passwords } = this.state;
-
     return (
       <div className="App">
-        {/* Render the passwords if we have them */}
-        {passwords.length ? (
-          <div>
-            <h1>5 Passwords.</h1>
-            <ul className="passwords">
-              {/*
-                Generally it's bad to use "index" as a key.
-                It's ok for this example because there will always
-                be the same number of passwords, and they never
-                change positions in the array.
-              */}
-              {passwords.map((password, index) =>
-                <li key={index}>
-                  {password}
-                </li>
-              )}
-            </ul>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Get More
-            </button>
-          </div>
-        ) : (
-          // Render a helpful message otherwise
-          <div>
-            <h1>No passwords :(</h1>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Try Again?
-            </button>
-          </div>
-        )}
+        <div className="AppPaper">
+          <h2>React Demo Apps</h2>
+	        <FaReact className="SplashIcon"/>
+        </div>
       </div>
     );
   }
