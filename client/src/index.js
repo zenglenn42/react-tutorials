9478e4ff4edfeda0a82980439231c564ec9c1641
zenglenn42 <gsnospampls@pobox.com>
2020-05-23 20:36:18 -0500

    add hamburger menu with persisten drawer left

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import PersistentDrawerLeft from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PersistentDrawerLeft />
   </React.StrictMode>,
   document.getElementById('root')
 );
