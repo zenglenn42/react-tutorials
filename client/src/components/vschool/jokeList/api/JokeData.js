dfbcc75dee23fab3da7e359cdc07a3e10e40299a
zenglenn42 <gsnospampls@pobox.com>
2020-06-13 17:36:25 -0500

    slightly less lame jokes

--- client/src/components/vschool/jokeList/api/JokeData.js
+++ client/src/components/vschool/jokeList/api/JokeData.js
@@ -1,11 +1,21 @@
 const JokeData = [
     {
-        question: "Why did the chicken cross the road?",
-        punchLine: "To get to the other side." 
-    },
-    {
         question: "What do you call a boomerang that doesn't work?",
         punchLine: "A stick." 
+    },
+    {
+        punchLine: 'A web designer is filling out a form: Age: 31 Height: 5’9” Eye color: #008000',
+        source: "https://short-funny.com/geek-jokes.php"
+    },
+    {
+        question: "A mother asks her son: 'Anton, do you think I’m a bad mom?'",
+        punchLine: "Son: 'My name is Paul.'" 
+    },
+    {
+        question: "What happens when eight hobbits get together?",
+        punchLine: "They turn into a hobbyte.",
+        source: "https://short-funny.com/geek-jokes.php"
+
     },
     {
         punchLine: 'Descartes walked into a bar and ordered a beer. “Want another?” asked the bartender. “I think not”, Descartes replied … then he disappeared.'
