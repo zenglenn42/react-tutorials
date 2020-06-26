5da35679c8b1484a4865cd429985dd908e85af9d
zenglenn42 <gsnospampls@pobox.com>
2020-06-26 08:48:04 -0500

    make tutorial list take up entire vertical space of browser so we don't get odd background color at bottom

--- client/src/components/TutorialList.js
+++ client/src/components/TutorialList.js
@@ -16,6 +16,7 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     maxWidth: 360,
     backgroundColor: theme.palette.background.paper,
+    height: '100vh'
   },
   nested: {
     paddingLeft: theme.spacing(4),
