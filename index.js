b03507e6fab8750ea2457f937516b05b010eed50
zenglenn42 <gsnospampls@pobox.com>
2020-06-29 20:43:29 -0500

    disable pw generator in server (cruft from start-up code)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const generatePassword = require('password-generator');
+// const generatePassword = require('password-generator');
 
 const app = express();
 
@@ -8,22 +8,24 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
-  const count = 5;
+// app.get('/api/passwords', (req, res) => {
+//   const count = 5;
 
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
+//   // Generate some passwords
+//   const passwords = Array.from(Array(count).keys()).map(i =>
+//     generatePassword(12, false)
+//   )
 
-  // Return them as json
-  res.json(passwords);
+//   // Return them as json
+//   res.json(passwords);
 
-  console.log(`Sent ${count} passwords`);
-});
+//   console.log(`Sent ${count} passwords`);
+// });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
+
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
@@ -31,4 +33,4 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log(`Password generator listening on ${port}`);
+console.log(`tutorial server listening on ${port}`);
