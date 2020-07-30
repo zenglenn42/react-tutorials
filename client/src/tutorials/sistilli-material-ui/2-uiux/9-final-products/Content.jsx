77f04154b03b9c6467f7db9d64f0eee7a277ea73
zenglenn42 <gsnospampls@pobox.com>
2020-07-30 12:10:36 -0500

    add key to grid items; throws warning otherwise

--- client/src/tutorials/sistilli-material-ui/2-uiux/9-final-products/Content.jsx
+++ client/src/tutorials/sistilli-material-ui/2-uiux/9-final-products/Content.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import CoffeeCard from './CoffeeCard'
 import { Grid } from '@material-ui/core'
+import { v4 as getKey } from 'uuid'
+import CoffeeCard from './CoffeeCard'
 import coffeeMakerList from './constants'
 
 const Content = () => {
     const getCoffeeMakerCard = (coffeeMakerObj) => {
         return (
-            <Grid item xs={12} sm={6} lg={4}>
+            <Grid item xs={12} sm={6} lg={4} key={getKey()}>
                 <CoffeeCard {...coffeeMakerObj} />
             </Grid>
         )
