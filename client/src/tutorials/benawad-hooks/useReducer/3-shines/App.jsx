825b00d06d0d9e2cffece9947e3f64eee174e100
zenglenn42 <gsnospampls@pobox.com>
2020-08-05 13:01:44 -0500

    add useReducer hook tutorial from BA; snapshot 3

---
+++ client/src/tutorials/benawad-hooks/useReducer/3-shines/App.jsx
@@ -0,0 +1,63 @@
+import React, { useReducer, useState } from 'react'
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'add-todo':
+            return {
+                todos: [
+                    ...state.todos,
+                    { text: action.text, completed: false }
+                ],
+                todoCount: state.todoCount + 1
+            }
+        case 'toggle-todo':
+            return {
+                todos: state.todos.map((t, idx) =>
+                    idx === action.idx ? { ...t, completed: !t.completed } : t
+                ),
+                todoCount: state.todoCount
+            }
+        default:
+            return state
+    }
+}
+
+export const App = () => {
+    const [{ todos, todoCount }, dispatch] = useReducer(reducer, {
+        todos: [],
+        todoCount: 0
+    })
+    const [text, setText] = useState('')
+    return (
+        <div>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    dispatch({ type: 'add-todo', text })
+                    setText('')
+                }}
+            >
+                <input
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    placeholder="Enter todo"
+                />
+            </form>
+            <div>number of todos: {todoCount}</div>
+            {todos.map((t, idx) => (
+                <div
+                    key={t.text}
+                    onClick={() => dispatch({ type: 'toggle-todo', idx })}
+                    style={{
+                        textDecoration: t.completed ? 'line-through' : 'none'
+                    }}
+                >
+                    {t.text}
+                </div>
+            ))}
+            {/* <pre>{JSON.stringify(todos, null, 2)}</pre> */}
+        </div>
+    )
+}
+
+export default App
